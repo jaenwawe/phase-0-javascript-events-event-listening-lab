4ffc8c5ff394e7f47cb9d7cc68b3e56581f376df
jaenwawe <nwawe.jae@gmail.com>
2021-07-28 10:47:11 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+input.addEventListener('click', clickAlert); 
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+
+function addingEventListener(){
+  
+    input.addEventListener('click', function() {alert('I was clicked!');
+    }); 
+    };
